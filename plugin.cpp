f07975632f91e572d69e9df627d257d391460e69
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2023-03-14 20:36:49 +0100

    Rename exprfilter.h to plugin.h

--- plugin.cpp
+++ plugin.cpp
@@ -1,6 +1,6 @@
 #include <vector>
 
-#include "exprfilter.h"
+#include "plugin.h"
 
 VS_EXTERNAL_API(void) VapourSynthPluginInit2(VSPlugin *plugin, const VSPLUGINAPI *vspapi) {
     vspapi->configPlugin("com.setsugen.dev", "setsugen", "Schizo Expr Plugin", VS_MAKE_VERSION(1, 0), VAPOURSYNTH_API_VERSION, 0, plugin);
